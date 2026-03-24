ee34af77d84dd101540be5558030dd7f76eb5631
lisaapatel <lisapatel1998@hotmail.com>
2026-03-23 17:56:30 -0700

    Update resume link on Contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -78,7 +78,7 @@ const Contact: NextPage = () => {
           </a>
           
           <a 
-            href="https://drive.google.com/file/d/1DgNjCX2A4Rf0XWq68-352GqQ8sIYrefD/view?usp=sharing" 
+            href="https://drive.google.com/file/d/1ayqG-fLxciouw6t_kGkF0ka_aNgejgaB/view?usp=sharing" 
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
